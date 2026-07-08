33ea099c2cce9dc6b554d7d0c6b2f4ec2e38eb05
Harsh singh chouhan <160889341+Harshrajput788@users.noreply.github.com>
2026-07-08 21:38:13 +0530

    Initial Commit

--- backend/services/user/src/app.ts
+++ backend/services/user/src/app.ts
@@ -10,14 +10,26 @@ import adminSellerProfileRouter from "./routes/profile/admin/adminSeller.routes.
 
 const app = express();
  
-const allowOrigin = process.env.FRONTEND_URL ? [process.env.FRONTEND_URL] : ["http://localhost:5173"]
+const allowOrigin =  [
+  process.env.FRONTEND_URL,
+  "http://localhost:5173",
+].filter(Boolean);
+
 
 app.use(helmet());
 app.use(express.json({ limit: "10kb" }));
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({
-  origin:allowOrigin,
-  credentials: true
+ origin(origin, callback) {
+      if (!origin) return callback(null, true);
+
+      if (allowOrigin.includes(origin)) {
+        return callback(null, true);
+      }
+
+      return callback(new Error(`Origin ${origin} not allowed by CORS`));
+    },
+    credentials: true,
 }));
 app.use(cookieParser());
 
