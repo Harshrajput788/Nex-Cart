2de3174a03c3fe88f1885d420d9deb01a1e0a575
Harsh singh chouhan <160889341+Harshrajput788@users.noreply.github.com>
2026-08-03 15:54:30 +0530

    Replace uploadSingle with upload in thumbnail routes

--- backend/services/product/src/routes/thumbnail/admin/admin.routes.ts
+++ backend/services/product/src/routes/thumbnail/admin/admin.routes.ts
@@ -3,7 +3,7 @@ import { createThumbnail,updateThumbnail,deleteThumbnail } from "../../../contro
 import { asyncHandlerFunction } from "../../../helper/asyncErrorHandler.js";
 import { authenticate } from "../../../middleware/authenticate.js";
 import { roleGuard } from "../../../middleware/rbac.js";
-import {  uploadSingle } from "../../../middleware/multer.js";
+import {  upload } from "../../../middleware/multer.js";
 import { validateRequest } from "../../../middleware/vaildate.js";
 import { createThumbnailSchema, updateThumbnailSchema } from "../../../helper/thumbnail.vaildtion.js";
 import { redisRateLimit } from "../../../middleware/ratelimit.js";
@@ -13,8 +13,8 @@ const router = Router();
 router.use(authenticate);
 router.use(roleGuard("ADMIN"));
 
-router.post("/", uploadSingle.single("thumbnail"),validateRequest(createThumbnailSchema),redisRateLimit({windowSec:60*15,max:10,prefix:"create-thumbnails-admin"}), asyncHandlerFunction(createThumbnail));
-router.put("/:thumbnailId", uploadSingle.single("thumbnail"),validateRequest(updateThumbnailSchema),redisRateLimit({windowSec:60*15,max:20,prefix:"create-thumbnails-admin"}), asyncHandlerFunction(updateThumbnail));
+router.post("/", upload.single("thumbnail"),validateRequest(createThumbnailSchema),redisRateLimit({windowSec:60*15,max:10,prefix:"create-thumbnails-admin"}), asyncHandlerFunction(createThumbnail));
+router.put("/:thumbnailId", upload.single("thumbnail"),validateRequest(updateThumbnailSchema),redisRateLimit({windowSec:60*15,max:20,prefix:"create-thumbnails-admin"}), asyncHandlerFunction(updateThumbnail));
 router.delete("/:thumbnailId",redisRateLimit({windowSec:60*15,max:30,prefix:"create-thumbnails-admin"}), asyncHandlerFunction(deleteThumbnail));
 
 export default router;
