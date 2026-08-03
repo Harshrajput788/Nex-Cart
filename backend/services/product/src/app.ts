447085521d88b94658b3dae7118f50e1196a4718
Harsh singh chouhan <160889341+Harshrajput788@users.noreply.github.com>
2026-08-03 22:13:31 +0530

    Update CORS middleware to support multiple origins
    Refactor CORS configuration to allow multiple origins and handle errors.

--- backend/services/product/src/app.ts
+++ backend/services/product/src/app.ts
@@ -20,7 +20,10 @@ config();
 
 const app = express();
 
-const allowedOrigins = process.env.FRONTEND_URL ? [process.env.FRONTEND_URL] : ["http://localhost:5173"];
+const allowOrigin =  [
+  process.env.FRONTEND_URL,
+  "http://localhost:5173",
+].filter(Boolean);
 
 app.use(async (req,res,next)=>{
  await connectDatabase();
@@ -30,7 +33,17 @@ app.use(async (req,res,next)=>{
 app.use(helmet());
 app.use(express.json({ limit: "20mb" }));
 app.use(express.urlencoded({ extended: true, limit: "20mb" }));
-app.use(cors({ credentials: true, origin: allowedOrigins }));
+app.use(cors({
+ origin(origin, callback) {
+      if (!origin) return callback(null, true);
+
+      if (allowOrigin.includes(origin)) {
+        return callback(null, true);
+      }
+      return callback(new Error(`Origin ${origin} not allowed by CORS`+ allowOrigin));
+    },
+    credentials: true,
+}));
 app.use(cookieParser());
 
 app.get("/health", (_, res) => {
