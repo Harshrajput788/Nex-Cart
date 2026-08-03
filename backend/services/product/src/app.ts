12efaf9b60e6a3ee710c1215c3f294a992cc374e
Harsh singh chouhan <160889341+Harshrajput788@users.noreply.github.com>
2026-08-03 22:36:13 +0530

    Implement CORS middleware for origin handling
    Added CORS middleware to handle allowed origins.

--- backend/services/product/src/app.ts
+++ backend/services/product/src/app.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import helmet from 'helmet';
+import cors from 'cors'
 import adminCategoryRouter from './routes/category/admin/admin.routes.js';
 import userCategoryRouter from './routes/category/user/user.routes.js'
 import cookieParser from 'cookie-parser';
@@ -19,6 +20,13 @@ config();
 
 const app = express();
 
+const allowOrigin =  [
+  process.env.FRONTEND_URL,
+  "http://localhost:5173",
+].filter(Boolean);
+
+
+
 app.use(async (req,res,next)=>{
  await connectDatabase();
  next();
@@ -27,6 +35,18 @@ app.use(async (req,res,next)=>{
 app.use(helmet());
 app.use(express.json({ limit: "20mb" }));
 app.use(express.urlencoded({ extended: true, limit: "20mb" }));
+app.use(cors({
+ origin(origin, callback) {
+      if (!origin) return callback(null, true);
+
+      if (allowOrigin.includes(origin)) {
+        return callback(null, true);
+      }
+
+      return callback(new Error(`Origin ${origin} not allowed by CORS`));
+    },
+    credentials: true,
+}));
 app.use(cookieParser());
 
 app.get("/health", (_, res) => {
