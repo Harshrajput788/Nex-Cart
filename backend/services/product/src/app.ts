0852ab103aea7dfbccde555de829f43715bc0506
Harsh singh chouhan <160889341+Harshrajput788@users.noreply.github.com>
2026-08-03 21:57:31 +0530

    Remove CORS middleware from application
    Removed CORS middleware configuration from app.

--- backend/services/product/src/app.ts
+++ backend/services/product/src/app.ts
@@ -34,21 +34,7 @@ app.use(async (req,res,next)=>{
 app.use(helmet());
 app.use(express.json({ limit: "20mb" }));
 app.use(express.urlencoded({ extended: true, limit: "20mb" }));
-app.use(
-  cors({
-    origin(origin, callback) {
-      if (!origin) return callback(null, true);
 
-      if (allowedOrigins.includes(origin)) {
-        return callback(null, true);
-      }
-      console.log(origin);
-
-      return callback(new Error(`Origin ${origin} not allowed by CORS`));
-    },
-    credentials: true,
-  })
-);
 app.use(cookieParser());
 
 app.get("/health", (_, res) => {
