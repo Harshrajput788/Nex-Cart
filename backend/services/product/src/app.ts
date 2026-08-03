69a547c802e833df2ff1e641be5dd85f9ddcf25c
Harsh singh chouhan <160889341+Harshrajput788@users.noreply.github.com>
2026-08-03 22:21:55 +0530

    Update CORS configuration with allowed origins

--- backend/services/product/src/app.ts
+++ backend/services/product/src/app.ts
@@ -20,10 +20,10 @@ config();
 
 const app = express();
 
-const allowOrigin =  [
-  process.env.FRONTEND_URL,
-  "http://localhost:5173",
-].filter(Boolean);
+const allowedOrigins = [
+  "https://nex-cart-vert.vercel.app",
+  "http://localhost:5173"
+];
 
 app.use(async (req,res,next)=>{
  await connectDatabase();
@@ -33,17 +33,18 @@ app.use(async (req,res,next)=>{
 app.use(helmet());
 app.use(express.json({ limit: "20mb" }));
 app.use(express.urlencoded({ extended: true, limit: "20mb" }));
-app.use(cors({
- origin(origin, callback) {
-      if (!origin) return callback(null, true);
-
-      if (allowOrigin.includes(origin)) {
-        return callback(null, true);
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error(`Origin ${origin} not allowed by CORS`));
       }
-      return callback(new Error(`Origin ${origin} not allowed by CORS ${process.env.FRONTEND_URL}`));
     },
     credentials: true,
-}));
+  })
+);
 app.use(cookieParser());
 
 app.get("/health", (_, res) => {
